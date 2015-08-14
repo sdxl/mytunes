081eafde7531ce47b45f3b3b421554a1a86ffac9
MKS-students <mks.students@hackreactor.com>
2015-08-14 15:52:55 -0500

    Restored lost information

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -4,18 +4,22 @@ var AppView = Backbone.View.extend({
   initialize: function(params){
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
+    this.songQueueView = new SongQueueView();
 
     // change:currentSong - this is Backbone's way of allowing you to filter events to
     // ONLY receive change events for the specific property, 'currentSong'
     this.model.on('change:currentSong', function(model){
       this.playerView.setSong(model.get('currentSong'));
     }, this);
+
+    //^^^ the above code does not work for songQueueView-related concerns
   },
 
   render: function(){
     return this.$el.html([
       this.playerView.$el,
-      this.libraryView.$el
+      this.libraryView.$el,
+      this.songQueueView.$el
     ]);
   }
 
